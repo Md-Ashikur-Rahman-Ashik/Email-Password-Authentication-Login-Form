55bdcdd09bf30b1b6b2aecdd6828ebfc5fca9165
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-04-03 11:46:23 +0600

    Simple form and daisyUI form created

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,40 @@
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    // console.log("Form Submitting")
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log("The email is:", email);
+    console.log("The password is:", password);
+  };
+
   return (
     <div>
-      <h3 className="text-3xl">Please Register</h3>
+      <div className="mx-auto md:w-1/2">
+        <h3 className="text-3xl mb-8">Please Register</h3>
+        <form onSubmit={handleRegister}>
+          <input
+            className="mb-4 w-3/4 py-2 px-4"
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+          />
+          <br />
+          <input
+            className="mb-4 w-3/4 py-2 px-4"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          <br />
+          <input
+            className="mb-4 btn btn-secondary text-white w-3/4"
+            type="submit"
+            value="Register"
+          />
+        </form>
+      </div>
     </div>
   );
 };
